9d283aeb5fe9f2cd8091e420aa80d5f5a19b2de9
babujani <sbabujani@gmail.com>
2021-09-21 13:03:42 +0000

    updated emptypage

--- src/components/emptyPage.js
+++ src/components/emptyPage.js
@@ -1,5 +1,5 @@
 import './emptyPage.css'
 
-const EmptyPage = () => <p>There is no history to show</p>
+const EmptyPage = () => <p className="noHistory">There is no history to show</p>
 
 export default EmptyPage
